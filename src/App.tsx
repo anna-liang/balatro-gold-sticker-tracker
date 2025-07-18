f9ec97b08c0da0e97a30650bb7b291c2701c8cbe
anna <annaliang56@gmail.com>
2025-07-18 19:24:07 -0400

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import styles from './App.module.css';
 import JokerLayout from 'components/JokerLayout/JokerLayout';
 
-// read joker-progress.json
-// if doesn't exist, create copy of joker-progress-template.json as joker-progress.json
-
-// read straight from joker-progress.json (requires user to copy template and create file)
-
-// for each joker from file
-//  if uri is empty: fill with '../../public/jokers/${jokerName}.png'
-
-// fetch('../store/joker-progress-template.json')
-//   .then((response) => response.json())
-//   .then((jsonResponse) => console.log(jsonResponse));
-
 function App() {
   return (
     <div className={styles.app}>
-      <div className="grid-cols-4 bg-red">
-        <JokerLayout />
+      <div className={styles.heading}>
+        <img
+          className={styles.icon}
+          src="/completionist-icon.jpg"
+          alt="completionist achievement icon"
+        />
+        <h1 className={styles.title}>Completionist++ Tracker</h1>
       </div>
+      <JokerLayout />
     </div>
   );
 }
